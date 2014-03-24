b7e8e7091a7cc943d8d6aedde3dad1f6f0fa4ecd
Aaron Silverman <aaron@aaronplusplus.com>
2014-03-24 18:01:22 -0400

    bugfix with database password; logging improvements; handle mysql comments with --

--- patch.js
+++ patch.js
@@ -10,8 +10,20 @@ var autopatcher = require('./lib/autoPatcher');
  */
 function exec() {
 
-    var profile = process.argv[2] || 'default';
-    var configFile = process.argv[3] || path.resolve(__dirname,'config.json');
+    var profile = process.argv[2];
+    var configFile = process.argv[3];
+
+    // If no config file but profile ends with .json or .config, user is only specifying a config file but no profile
+    if (profile.match(/\.(json|config)$/i)) {
+        configFile = profile;
+        profile = null;
+    }
+
+    profile = profile || 'default';
+    configFile = configFile || path.resolve(__dirname,'config.json');
+
+
+    // Read in entire configuration
 
     if (!fs.existsSync(configFile)) {
         console.error('Missing config.json file');
@@ -19,6 +31,8 @@ function exec() {
     }
 
     var universalConfig = JSON.parse(fs.readFileSync(configFile));
+
+    // Load the config for the given profile
 
     if (!universalConfig.profiles[profile]) {
         console.err('Invalid profile '+profile+'. Specify one of the following available profiles: '+Object.keys(universalConfig.profiles || {}).join(', '));
--- test/patchdirs/minor/patch_0000_2__data.sql
+++ test/patchdirs/minor/patch_0000_2__data.sql
@@ -2,7 +2,8 @@
 INSERT INTO people (name)
 VALUES ('John Smith');
 
+-- This should be cool
 INSERT INTO colors (person_id, color)
-SELECT id, 'red' FROM people WHERE name = 'John Smith'
+SELECT id, 'red' FROM people WHERE name = 'John Smith' --love that color!
 UNION ALL
 SELECT id, 'blue' FROM people WHERE name = 'John Smith';

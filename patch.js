615780cb4a82960bd955735bd78e6c9901f73dd0
jonathan-fulton <jonathan@videoblocks.com>
2014-10-02 15:54:15 -0400

    [Refactor]: refactoring most of autopatcher according to Clean Code principles

--- patch.js
+++ patch.js
@@ -3,54 +3,27 @@
 var fs = require('fs');
 var path = require('path');
 
-var autopatcher = require('./lib/autoPatcher');
+var patchCommandLineArguments = require('./lib/patchCommandLineArguments');
+var Autopatcher = require('./lib/autopatcher');
+var AutopatcherConfiguration = require('./lib/autopatcherConfiguration');
 
 /**
  * Reads in configuration and calls the autopatcher
  */
 function exec() {
 
-    var profile = process.argv[2];
-    var configFile = process.argv[3];
+    var commandLineArguments = new patchCommandLineArguments(process.argv);
 
-    // If no config file but profile ends with .json or .config, user is only specifying a config file but no profile
-    if (profile && profile.match(/\.(json|config)$/i)) {
-        configFile = profile;
-        profile = null;
+    try {
+        var autopatcherConfiguration = new AutopatcherConfiguration(commandLineArguments);
+    }
+    catch (err) {
+        console.log(err.message);
+        process.exit(err.code);
     }
 
-    profile = profile || 'default';
-    configFile = configFile || path.resolve(__dirname,'config.json');
-
-
-    // Read in entire configuration
-
-    if (!fs.existsSync(configFile)) {
-        console.error('Missing config.json file');
-        process.exit(1);
-    }
-
-    var universalConfig = JSON.parse(fs.readFileSync(configFile));
-
-    // Load the config for the given profile
-
-    if (!universalConfig.profiles[profile]) {
-        console.error('Invalid profile '+profile+'. Specify one of the following available profiles: '+Object.keys(universalConfig.profiles || {}).join(', '));
-        process.exit(2);
-    }
-
-    var config = universalConfig.profiles && universalConfig.profiles.default || {};
-    var profileConfig = universalConfig.profiles && universalConfig.profiles[profile] || {};
-
-    // override defaults with anything profile specific
-    Object.keys(profileConfig).forEach(function(key) {
-        config[key] = profileConfig[key];
-    });
-
-    // include directory of configuration file for resolving paths
-    config.configDir = path.dirname(configFile);
-
-    autopatcher.patch(config);
+    var autopatcher = new Autopatcher(autopatcherConfiguration);
+    autopatcher.patch();
 }
 
 exec();
